c6792470d48390b52a0e930e5b3567d6535349ae
Leo23-23 <58565201+Leo23-23@users.noreply.github.com>
2021-05-08 14:30:41 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,67 +1,12 @@
 import { createStore } from 'vuex'
-import axios from 'axios'
 
 export default createStore({
   state: {
-    pokemons: [],
-    pokemons1: [],
-    apiUrl: 'http://pokeapi.co/api/v2/pokemon',
-    imageUrl : 'https://pokeres.bastionbot.org/images/pokemon/',
-    
-  },
-  getters : {
-    pokemons (state){
-      return state.pokemons
-    },
-    pokemons1 (state){
-      return state.pokemons1
-    },
   },
   mutations: {
-    FETCHDATA(state, pokemons){
-      state.pokemons=pokemons
-    },
-    FETCHDATA1(state, pokemons1){
-      state.pokemons1=pokemons1
-    },
   },
   actions: {
-    act_fetchdata ({commit}){
-      axios
-        .get (this.state.apiUrl)
-        .then( response =>  {
-           commit('FETCHDATA', response.data)})
-        .catch((error) => {
-          console.log(error);
-        })
-    },
-    act_fetchdata1 ({commit}){
-      axios
-        .get (this.state.apiUrl)
-        .then( response =>  {
-           commit('FETCHDATA1', response.data.results)})
-        .catch((error) => {
-          console.log(error);
-        })
-    },
-    act_next ({commit}){
-      axios
-        .get (this.state.pokemons.next)
-        .then( response =>  {
-           commit('FETCHDATA', response.data)})
-        .catch((error) => {
-          console.log(error);
-        })
-    },
-    act_prev ({commit}){
-      axios
-        .get (this.state.pokemons.previous)
-        .then( response =>  {
-           commit('FETCHDATA', response.data)})
-        .catch((error) => {
-          console.log(error);
-        })
-    },
   },
-
+  modules: {
+  }
 })
